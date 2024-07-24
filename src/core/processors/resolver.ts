c79a0f8952dab1d75cba18b61ddbd24cad9d8120
Ramsey <someramseyy@gmail.com>
2024-07-24 15:55:34 +0200

    Making the resolver handle reference

--- src/core/processors/resolver.ts
+++ src/core/processors/resolver.ts
@@ -1,51 +1,89 @@
-import { Expression, ResolvedExpression, UnresolvedExpression } from "../../expression";
+import { Expression, ReferenceExpression, ResolvedExpression, UnresolvedExpression, ValueLiteralExpression } from "../../expression";
 import { Instruction } from "../../instruction";
-import { ArrayItem, ArrayMetaData, CompoundMetaData, GroupMetaData, NodeMetaData } from "../../meta";
+import { ArrayItem, ArrayMetaData, BlockMetaData, CompoundMetaData, GroupMetaData, NodeMetaData } from "../../meta";
 import { ProcessorError, ProcessorResult } from "../processor";
 
 
-function resolveExpression(expression: UnresolvedExpression): ResolvedExpression {
-    const resolveGroup = (data: GroupMetaData<UnresolvedExpression>): GroupMetaData<ResolvedExpression> => {
-        return {
-            meta: "group",
-            value: resolveExpression(data.value)
-        };
-    };
 
-    const resolveArray = (data: ArrayMetaData<UnresolvedExpression>): ArrayMetaData<ResolvedExpression> => {
-        return {
-            meta: "array",
-            value: data.value.map<ArrayItem<ResolvedExpression>>(item => {
-                if (item.kind == "single") {
-                    return {
-                        ...item,
-                        expression: resolveExpression(item.expression)
-                    }
-                } else {
-                    return {
-                        ...item,
-                        from: resolveExpression(item.from),
-                        to: resolveExpression(item.to)
-                    }
-                }
-            })
-        }
-    };
 
-    if (expression.kind == "literal") {
-        switch (expression.data.meta) {
-            case "group": return { data: resolveGroup(expression.data) }
-            case "array": return { data: resolveArray(expression.data) }
-            case "block": null //TODO
-        }
-
-        return { data: expression.data };
-    }
-}
 
 export function resolve(instructions: Instruction<UnresolvedExpression>[]): ProcessorResult<Instruction<ResolvedExpression>[]> {
     const output: Instruction<ResolvedExpression>[] = [];
     const errors: ProcessorError[] = [];
+
+    const resolveExpression = (expression: UnresolvedExpression, premature: boolean): ResolvedExpression => {
+        const unwrapMetaData = (data: NodeMetaData<UnresolvedExpression>): NodeMetaData<ResolvedExpression> => {
+            const resolveGroup = (data: GroupMetaData<UnresolvedExpression>): GroupMetaData<ResolvedExpression> => {
+                return {
+                    meta: "group",
+                    value: resolveExpression(data.value, premature)
+                };
+            };
+
+            const resolveArray = (data: ArrayMetaData<UnresolvedExpression>): ArrayMetaData<ResolvedExpression> => {
+                return {
+                    meta: "array",
+                    value: data.value.map<ArrayItem<ResolvedExpression>>(item => {
+                        if (item.kind == "single") {
+                            return {
+                                ...item,
+                                expression: resolveExpression(item.expression, premature)
+                            }
+                        } else {
+                            return {
+                                ...item,
+                                from: resolveExpression(item.from, premature),
+                                to: resolveExpression(item.to, premature)
+                            }
+                        }
+                    })
+                }
+            };
+
+            const resolveBlock = (data: BlockMetaData<UnresolvedExpression>): BlockMetaData<ResolvedExpression> => {
+                return {
+                    meta: "block",
+                    value: data.value.map(property => ({
+                        key: property.key,
+                        expression: resolveExpression(property.expression, premature)
+                    }))
+                }
+            }
+
+            switch (data.meta) {
+                case "group": return resolveGroup(data)
+                case "array": return resolveArray(data)
+                case "block": return resolveBlock(data)
+            }
+
+            return data;
+        }
+
+        const resolveReference = (reference: ReferenceExpression): NodeMetaData<ResolvedExpression> => {
+            instructions.forEach(instruction => {
+                if (instruction.identifier == reference.name) {
+                    const expression = resolveExpression(instruction.expression, false);
+
+
+
+
+
+
+
+
+                }
+            })
+        }
+
+
+
+
+        if (expression.kind == "literal") {
+            return { data: unwrapMetaData(expression.data) };
+        } else {
+            return { data: resolveReference(expression) };
+        }
+    }
 
     const resolveAssignment = (instruction: Instruction<UnresolvedExpression>) => {
 

61623d95b3f607480df69c5347c57a065cee9751
Ramsey <someramseyy@gmail.com>
2024-07-31 17:25:58 +0200

    Removed all the trash and clutter from the resolver and started reimplementing it. Currently trying to get it to resolve value literals

--- src/core/processors/resolver.ts
+++ src/core/processors/resolver.ts
@@ -1,213 +1,102 @@
-import { Expression, ReferenceExpression, ValueLiteralExpression } from "../../expression";
-import { Definition, ThemeDefinition, UnresolvedDefinition } from "../../instruction";
-import { ArrayMetaData, BlockMetaData, GroupMetaData, NodeMetaData, PrimeMetaData } from "../../meta";
+import { Range } from "../../range";
+import { ParseResult } from "./parser";
+import { Definition, UnresolvedDefinition } from "../../definition";
+import { Expression, ValueLiteralExpression } from "../../expression";
+import { ArrayItemStep as ArrayItemSteps, ArrayMetaData, BlockMetaData, GroupMetaData, NodeMetaData, UnresolvedArrayMetaData, UnresolvedBlockMetaData, UnresolvedGroupMetaData } from "../../meta";
 import { ProcessorError, ProcessorResult } from "../processor";
 
-export type ResolvedArrayData = ResolvedData[];
-export type ResolvedBlockData = {
-    [key: string]: ResolvedData;
-};
-
-export type ResolvedData = PrimeMetaData | ResolvedBlockData | ResolvedArrayData;
-
-export function resolve(definitions: UnresolvedDefinition[]): ProcessorResult<Definition[]> {
+export function resolve(parseResult: ParseResult): ProcessorResult<Definition[]> {
     const output: Definition[] = [];
     const errors: ProcessorError[] = [];
 
-    const traceReference = (reference: ReferenceExpression): ValueLiteralExpression => {
-        const lookup = (reference: ReferenceExpression): ValueLiteralExpression => {
-            for (const definition of definitions) {
-                if (definition.type == "assignment" && definition.identifier == reference.name) {
-                    if (definition.body.kind == "literal") {
-                        return definition.body;
-                    }
-
-                    return lookup(definition.body);
-                }
+    const resolveGroup = (group: UnresolvedGroupMetaData): GroupMetaData => {
+        return {
+            meta: "group",
+            value: {
+                item: resolveExpression(group.value.expression)
             }
-
-            throw new ProcessorError(`Reference could not be found: ${reference.name}`, reference.range);
         };
-
-        // //TODO: check for cross references
-
-        let target: ValueLiteralExpression = lookup(reference);
-        let path: string[] = [reference.name];
-
-        const follow = (expression: Expression) => {
-            switch (expression.kind) {
-                case "literal": target = expression; break;
-                case "reference": target = traceReference(expression); break;
-            }
-        }
-
-        const followAccessor = (key: string) => {
-            if (target.data.meta !== "block") {
-                throw new ProcessorError(`Cannot use acessor on non-block data`, reference.range);
-            }
-
-            for (const property of target.data.value) {
-                if (property.key == key) {
-                    follow(property.expression);
-                    path.push(key);
-                    return;
-                }
-            }
-
-            throw new ProcessorError(`Property '${key}' does not exist on '${path.join(".")}'`, reference.range);
-        };
-
-        const followIndexer = (indexExpression: Expression) => {
-            let indexData: NodeMetaData;
-
-            if (indexExpression.kind == "reference") {
-                indexData = traceReference(indexExpression).data;
-            } else {
-                indexData = indexExpression.data
-            }
-
-            switch (target.data.meta) {
-                case "array": {
-                    if (indexData.meta !== "number") {
-                        throw new ProcessorError(`'${indexData.meta} cannot be used as an index on an array`, reference.range);
-                    }
-
-                    const index = indexData.value;
-                    const array = target.data.value;
-
-                    if (index < 0 || index >= array.length) {
-                        throw new ProcessorError(`Index out of bounds, index (${index}) of array (${array})`, reference.range);
-                    }
-
-                    const item = array[index];
-
-                    if (item.kind == "range") {
-                        throw new ProcessorError(`Cannot reference a ranged array item`, reference.range);
-                    }
-
-                    follow(item.expression);
-                    path.push(`[${index}]`);
-                } break;
-
-                case "block": {
-                    if (indexData.meta !== "string") {
-                        throw new ProcessorError(`'${indexData.meta} cannot be used as an index on an object`, reference.range);
-                    }
-
-                    const key = indexData.value;
-                    const block = target.data.value as BlockMetaData["value"];
-
-                    for (const property of block) {
-                        if (property.key == key) {
-                            follow(property.expression);
-                            path.push(key);
-                            return;
-                        }
-                    }
-
-                    throw new ProcessorError(`Property '${key}' does not exist on '${path.join(".")}'`, reference.range);
-                };
-
-                default: throw new ProcessorError(`Type is not indexable, indexer can only be used on arrays and blocks`, reference.range);
-            }
-
-        }
-
-        for (const extender of reference.extenders) {
-            switch (extender.kind) {
-                case "acessor": followAccessor(extender.name); break;
-                case "indexer": followIndexer(extender.expression); break;
-            }
-        }
-
-        return target;
     };
 
-    const unwrapReference = (expression: Expression): ValueLiteralExpression => {
-        if (expression.kind === "reference") {
-            return traceReference(expression);
-        }
+    const resolveArray = (array: UnresolvedArrayMetaData): ArrayMetaData => {
+        const value: NodeMetaData[] = [];
 
-        return expression;
-    };
-
-    const resolveGroup = (expression: ValueLiteralExpression<GroupMetaData>): ResolvedData => {
-        return resolveExpresion(expression.data.value);
-    };
-
-    const resolveArray = (expression: ValueLiteralExpression<ArrayMetaData>): ResolvedArrayData => {
-        return expression.data.value.map(item => {
+        array.value.forEach((item) => {
             if (item.kind == "single") {
-                return resolveExpresion(item.expression);
+                value.push(resolveExpression(item.expression));
+                return;
             }
 
-            const rangeBegin = unwrapReference(item.from);
-            const rangeEnd = unwrapReference(item.to);
+            const fromValue = resolveExpression(item.from);
+            const toValue = resolveExpression(item.to);
 
-            if (rangeBegin.data.meta !== rangeEnd.data.meta) {
-                throw new ProcessorError("Ranged array bounds must be of the same type", expression.range);
+            if (fromValue.meta !== toValue.meta) {
+                throw new ProcessorError("Ranged item bounds must be of the same kind", item.range);
             }
 
-            switch (rangeBegin.data.meta) { //TODO: generate the values for ranged items
-                case "number": break;
-                case "hex": break;
+            const fabricateNumberRange = (from: number, to: number, steps: ArrayItemSteps) => {
+                if (from > to) {
+                    throw new ProcessorError("Invalid range bounds, lower bound of the range must be less than the upper bound", item.range);
+                }
+
+                const step = steps !== "auto" ?
+                    ((to - from) / steps) : 1;
+
+                for (let i = from; i <= to; i += step) {
+                    value.push({ meta: "number", value: i });
+                }
+            };
+
+            const fabricateHexRange = (from: string, to: string, steps: ArrayItemSteps) => {
+                
             }
 
-            throw new ProcessorError("Invalid type for ranged array bounds", expression.range);
-        });
+            switch (fromValue.meta) {
+                case "number": fabricateNumberRange(fromValue.value, toValue.value as number, item.steps); break;
+                case "hex": fabricateHexRange(fromValue.value, toValue.value as string, item.steps); break;
+
+                default: throw new ProcessorError("Invalid type for ranged item bounds", item.range);
+            }
+        })
+
+        return { meta: "array", value };
     };
 
-    const resolveBlock = (expression: ValueLiteralExpression<BlockMetaData>): ResolvedBlockData => {
-        return expression.data.value.reduce((acc: ResolvedBlockData, property) => {
-            acc[property.key] = resolveExpresion(property.expression);
+    const resolveBlock = (block: UnresolvedBlockMetaData): BlockMetaData => {
+        const value: { [key: string]: NodeMetaData } = block.value.reduce((acc, property) => {
+            acc[property.key] = resolveExpression(property.expression);
             return acc;
         }, {});
-    };
 
-    const resolveExpresion = (expression: Expression): ResolvedData => {
-        const resolveData = (valueExpression: ValueLiteralExpression): ResolvedData => {
-            switch (valueExpression.data.meta) {
-                case "block": return resolveBlock(valueExpression as ValueLiteralExpression<BlockMetaData>);
-                case "array": return resolveArray(valueExpression as ValueLiteralExpression<ArrayMetaData>);
-                case "group": return resolveGroup(valueExpression as ValueLiteralExpression<GroupMetaData>);
+        return { meta: "block", value };
+    }
+
+    const resolveExpression = (expression: Expression): NodeMetaData => {
+        const resolveLiteral = (literal: ValueLiteralExpression): NodeMetaData => {
+            switch (literal.data.meta) {
+                case "group": return resolveGroup(literal.data);
+                case "array": return resolveArray(literal.data);
+                case "block": return resolveBlock(literal.data);
             }
 
-            return valueExpression.data
+            return literal.data;
         };
 
         switch (expression.kind) {
-            case "literal": return resolveData(expression);
-            case "reference": return resolveData(traceReference(expression));
+            case "literal": return resolveLiteral(expression);
+            case "reference": throw new Error("Not implemented");
         }
     };
 
+    parseResult.definitions.forEach((definition: UnresolvedDefinition) => {
+        const signature = definition.signature;
 
+        switch (signature.type) {
+            case "theme": {
 
-    const resolveThemeDefinition = (definition: ThemeDefinition<Expression>) => {
-        if (definition.body.kind !== "literal" || definition.body.data.meta !== "block") {
-            throw new ProcessorError("Expected body to be a block literal", definition.body.range);
+            } break;
         }
-
-        const body = resolveBlock(definition.body as ValueLiteralExpression<BlockMetaData>);
-
-        output.push({
-            type: "theme",
-            identifier: definition.identifier,
-            body
-        });
-    };
-
-
-
-    for (const definition of definitions) {
-        try {
-            switch (definition.type) {
-                case "theme": resolveThemeDefinition(definition); break;
-            }
-        } catch (error) {
-            errors.push(error as ProcessorError);
-        }
-    }
+    });
 
     return { output, errors };
 }
